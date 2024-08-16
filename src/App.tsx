b37a948ea5c51ce80796e853b2cd956deaed0d3b
DuetoPark <inhye.park.dev@gmail.com>
2024-08-17 00:51:56 +0900

    💄 base 설정, color theme 선언

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">ㅁㄴㅁㅇㄴㅇㄴ</div>;
+  return (
+    <div className="App">
+      <div className="example"></div>
+    </div>
+  );
 }
 
 export default App;
