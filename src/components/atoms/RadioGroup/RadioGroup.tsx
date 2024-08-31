84510689af4f42d160db3425c51ceee0c6982ad0
DuetoPark <inhye.park.dev@gmail.com>
2024-08-31 17:48:39 +0900

    ♻️ [refactor] RadioGroup 구조 변경

--- src/components/atoms/RadioGroup/RadioGroup.tsx
+++ src/components/atoms/RadioGroup/RadioGroup.tsx
@@ -1,14 +1,9 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, ReactNode, useState } from "react";
 import * as PrimitiveRadioGroup from "@radix-ui/react-radio-group";
 
 import "./_radioGroup.scss";
 
-import Radio from "./Radio";
-import { RadioItemProps } from "./Radio/Radio";
-
 export interface RadioGroupProps {
-  /** radio items 데이터 */
-  items: RadioItemProps[];
   /** radio group 값 */
   value?: string;
   /** radio 상호작용 */
@@ -19,29 +14,22 @@ export interface RadioGroupProps {
   name: string;
   /** 키보드 탐색 */
   loop?: boolean;
-  /** radio 크기 */
-  size?: RadioItemProps["size"];
   /** radio 이벤트 */
   onChange?: (e?: FormEvent<HTMLInputElement>) => void;
+  /** radio 그룹의 custom Children */
+  children: ReactNode;
 }
 
 const RadioGroup: React.FC<RadioGroupProps> = ({
-  items,
   value,
   disabled,
   required,
   name,
   loop,
-  size = "normal",
   onChange,
+  children,
 }) => {
   const [selected, setSelected] = useState<RadioGroupProps["value"]>(value);
-
-  useEffect(() => {
-    if (!selected && items.length > 0) {
-      setSelected(items[0].value);
-    }
-  }, [selected, items]);
 
   const handleValueChange = (newValue: string) => {
     setSelected(newValue);
@@ -57,23 +45,9 @@ const RadioGroup: React.FC<RadioGroupProps> = ({
       name={name}
       loop={loop}
       onValueChange={handleValueChange}
+      asChild
     >
-      {items.length > 0 &&
-        items.map(({ id, value, text }) => (
-          <div key={id} className="radio--item">
-            <Radio
-              id={id}
-              className="radio--item-radio"
-              value={value}
-              disabled={disabled}
-              size={size}
-            />
-
-            <label className="radio--item-text" htmlFor={id}>
-              {text}
-            </label>
-          </div>
-        ))}
+      {children}
     </PrimitiveRadioGroup.Root>
   );
 };
