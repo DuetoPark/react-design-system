d6108e8661809d7a814e73866d99b2c3ca10ebaf
DuetoPark <inhye.park.dev@gmail.com>
2024-09-03 21:56:30 +0900

    💄 [compo] Overlay 구현

---
+++ src/components/atoms/Overlay/Overlay.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./_overlay.scss";
+
+const Overlay: React.FC = () => {
+  return <div className="overlay" aria-hidden={true}></div>;
+};
+
+export default Overlay;
