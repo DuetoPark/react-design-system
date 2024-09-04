bdce745d6e31aab8f96aed33fe1b2bdeb265ca7a
DuetoPark <inhye.park.dev@gmail.com>
2024-09-04 17:24:27 +0900

    💄 [compo] Mask 구현

---
+++ src/components/atoms/Mask/Mask.tsx
@@ -0,0 +1,21 @@
+import React, { HTMLAttributes, PropsWithChildren } from "react";
+import classNames from "classnames";
+
+import "./_mask.scss";
+
+export interface MaskPropsType extends HTMLAttributes<HTMLDivElement> {
+  /** mask 각도 */
+  rotate: "top" | "right" | "bottom" | "left";
+}
+
+const Mask: React.FC<MaskPropsType> = ({ rotate, className, ...props }) => {
+  return (
+    <div
+      className={classNames("mask", `mask--${rotate}`, className)}
+      {...props}
+      aria-hidden
+    ></div>
+  );
+};
+
+export default Mask;
